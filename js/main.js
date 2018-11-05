5f9480d10e1cfc22c99ca488b9267120289af968
KonstantinHM <43475204+KonstantinHM@users.noreply.github.com>
2018-11-05 17:53:37 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,88 @@
+// main-slider function
+jQuery(document).ready(function($) {
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		items:1,
+		dots:true,
+		nav: false,
+		mouseDrag:false,
+		touchDrag:false,
+		autoplay:true,
+		autoplayTimeout:5000,
+		autoplaySpeed:1000
+	});
+});
+
+// main-slider arrow-buttons function
+
+jQuery(document).ready(function($) {
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+
+	$('.customNextBtn').click(function() {
+		owl.trigger('next.owl.carousel', [600]);
+	})
+	$('.customPrevBtn').click(function() {
+		owl.trigger('prev.owl.carousel', [600]);
+	});
+});
+
+
+// portfolio category function
+
+$('.tab-nav li').click(function(){
+	var get_id = this.id;
+	var get_current = $('.image-column .' + get_id);
+
+	$('.tab-nav ul .activeTab').removeClass('activeTab');
+	$(this).addClass('activeTab');
+	$('.portfolio_image-item').not(get_current).slideUp(500);
+	get_current.slideDown(500);
+});
+
+$('#all').click(function () {
+	$('.portfolio_image-item').slideDown(500);
+});
+
+// portfolio "Show more" - button function
+
+function showMoreBtn() {
+	var portfolio = document.getElementsByClassName('portfolio-image_wrapper');
+	var btnText = document.getElementsByClassName('portfolio_load-more_btn');
+
+	if (portfolio[0].style.height == 'auto') {
+		portfolio[0].style.height = '1350.63px';
+		btnText[0].innerHTML = 'Load more';
+	} else {
+		portfolio[0].style.height = 'auto';
+		btnText[0].innerHTML = 'Hide';
+	}
+};
+
+
+// "about" page skill-bar function
+
+$(".skill-percent").viewportChecker({
+	callbackFunction:function(){
+		$('.skill-bar').each(function(){
+			$(this).find('.skill-line').animate({
+				width:$(this).attr('data-percent')
+			}, 2000);
+		});
+	}
+});
+
+
+// portfolio details page - share post button function 
+
+function openShareList() {
+	var shareList = document.getElementById('share-post_socials-list');
+	var activeBtn = document.getElementsByClassName('share-btn');
+	if (shareList.style.display == 'block') {
+		shareList.style.display = 'none';
+		activeBtn[0].classList.remove('share-btn_active');
+	} else {
+		shareList.style.display = 'block';
+		activeBtn[0].classList.add('share-btn_active');
+	}
+};
